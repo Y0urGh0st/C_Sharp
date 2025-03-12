19ede0583d9dd24c92d73a39c3706e08fc85f32c
Y0urGh0st <kyrylo.kovalenko@hitek-games.com>
2025-03-12 15:57:30 +0200

    lab1

---
+++ Lab1/Lab1/Product.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Product
+    {
+        private string _name;
+        private decimal _price;
+        private int _quantity;
+   
+        public Product(string _name, decimal _price, int _quantity)
+        {
+            Name = _name;
+            Price = _price;
+            this._quantity = _quantity;
+
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Назва товару не може бути порожньою");
+                _name = value;
+            }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentException("Ціна не може бути від'емною!");
+                _price = value;
+
+            }
+
+
+        }
+        public int Quantity
+        {
+             get; private set; 
+        }
+        public decimal TotalValue
+        {
+            get { return _quantity * _price; }
+        }
+        public void Restock(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Кількість товарів повинна бути додатною");
+                return;
+            }
+            _quantity += amount;
+        }
+        public void Sell(int amount)
+        {
+            if(amount > _quantity)
+            {
+                Console.WriteLine("Недостатньо товару на складі");
+                return;
+            }
+            if(amount <= 0)
+            {
+                Console.WriteLine("Кількість продажу повина бути дадотною");
+                return;
+            }
+            _quantity -= amount;
+        }
+        public string GetInfo()
+        {
+            return $"Товар:  {Name}, Ціна: {Price}, Кількість: {Quantity}, Загальна вартість: {TotalValue} грн ";
+        }
+
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product apple = new Product("Яблуко", 5, 100);
+            Console.WriteLine(apple.GetInfo());
+            // Виведе: Товар: Яблуко, Ціна: 5 грн, Кількість: 100, Загальна вартість: 500 грн
+
+            apple.Sell(20);
+            Console.WriteLine(apple.GetInfo());
+            // Виведе: Товар: Яблуко, Ціна: 5 грн, Кількість: 80, Загальна вартість: 400 грн
+
+            apple.Restock(50);
+            Console.WriteLine(apple.GetInfo());
+            // Виведе: Товар: Яблуко, Ціна: 5 грн, Кількість: 130, Загальна вартість: 650 грн
+
+            apple.Price = 7; // Зміна ціни товару
+            Console.WriteLine(apple.GetInfo());
+            // Виведе: Товар: Яблуко, Ціна: 7 грн, Кількість: 130, Загальна вартість: 910 грн
+
+            apple.Name = "Зелене яблуко"; // Перейменування товару
+            Console.WriteLine(apple.GetInfo());
+            // Виведе: Товар: Зелене яблуко, Ціна: 7 грн, Кількість: 130, Загальна вартість: 910 грн
+            try
+            {
+                apple.Price = -10; // Повинно викликати помилку
+            }
+            catch ( Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                apple.Name = "";    // Повинно викликати помилку
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            apple.Sell(200);    // Повинно вивести: "Недостатньо товару на складі!"
+
+        }
+    }
+}
